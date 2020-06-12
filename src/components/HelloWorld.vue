65cd1371a0b1af5f6e01e9c45489bdcae1689899
Seybel <seyibello31@gmail.com>
2020-06-12 18:29:16 +0100

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,55 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<template> 
+  <div>
+    <div class="chart-wrapper">
+      <apexchart 
+        width="800" type="bar" 
+        :options="options" :series="series">
+      </apexchart>
+    </div>
+    <hr>
+    <div class="chart-wrapper">
+      <apexchart 
+        width="800" type="line" 
+        :options="options" :series="series">
+      </apexchart>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+ export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data: () => ({
+    options: { 
+        chart: { 
+          id: 'vuechart-example'
+        },        
+        xaxis: { 
+          categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        },
+        title: {
+          text: 'Monthly Stock Pricing',
+          align: 'center',
+          style: {
+            fontSize:  '20px',
+          },
+        },
+        colors: ['#00897b']
+      },      
+      series: [
+        {
+          name: 'series-1',
+          data: [55, 62, 89, 66, 98, 72, 101, 75, 94, 120, 117, 139]
+        }
+      ]
+  })
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+div.chart-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center 
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '@/plugins/apexcharts'
+
 Vue.config.productionTip = false
 
 new Vue({
